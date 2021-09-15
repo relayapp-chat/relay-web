ddf7cca89beab41996ccdfaa5405c91cdad15fb7
reesericci <reesericci@nyctech.cloud>
2021-09-15 15:10:11 -0500

    Co-authored-by: CoopGroup <CooperSma@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let server = window.prompt("What server would you like to connect to? \n(leave blank for default)")
+console.log(server)
 if(server = String(null)) {
   server = "localhost:8080"
 }
@@ -9,8 +10,7 @@ function sendMessage() {
     const textbox = document.getElementById("textbox")
 
     console.log(textbox.value);
-    socket.send(textbox.value);
-    document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + textbox.value + "\n"
+    socket.send(`{ "channel": "#general", "message": "${textbox.value}" }`);
     textbox.value = ""
     
 
@@ -34,10 +34,3 @@ socket.addEventListener('message', function (event) {
     console.log(event.data);
     document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + event.data;
 });
-
-var input = document.getElementById("textbox");
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-   console.log("hi")
-  }
-}) 
