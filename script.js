611b7f794e878396db20b97c86e898f312104533
reesericci <reesericci@nyctech.cloud>
2021-10-08 18:30:44 -0500

    initial emoji support!

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+window.onload = function() {
+document.querySelector('emoji-picker').addEventListener('emoji-click', event => {
+    document.getElementById("textbox").value = document.getElementById("textbox").value + event.detail.unicode;
+    focusTextbox()
+  });
+}
 let server = window.prompt("What server would you like to connect to? \n(leave blank for default)")
 console.log(server)
 if(server == null) {
   server = "localhost:8080"
 }
 const socket = new WebSocket('ws://' + server);
+
+function focusTextbox() {
+  document.getElementById("textbox").focus()
+}
 
 
 function sendMessage() {
@@ -24,13 +34,8 @@ function enter(value) {
   }
   
 }
-// Create WebSocket connection.
 
-
-// Connection opened
-
-// Listen for messages
-socket.addEventListener('message', function (event) {
+socket.addEventListener('message', function onMessage(event) {
     console.log(event.data);
     document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + event.data;
     var audio = new Audio('send.mp3');
@@ -47,7 +52,7 @@ function toggleChannelSlideover() {
 } 
 
 
-socket.onclose = function () {
+socket.onclose = function onclose() {
   alert("Server Disconnected");
   location.reload();
 };
